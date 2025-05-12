bfade77a8ae7933da18f2b7fd48afac81774841c
Maria Thaynara Soares Falcão <dig.thaynarafalcao@opovo.local>
2025-05-12 12:56:10 -0300

    fix: correções de validação JS

--- script.js
+++ script.js
@@ -12,18 +12,24 @@ form.addEventListener('submit', (event) => {
     checkInputEmail();
     checkInputPassword();
 
-    if (isFormValid()) {
+    if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
         alert("Form submitted successfully!");
+        form.reset(); //Limpa o fomulário após o envio
+        clearFormStates(); // Limpa os estados do formulário
     }
 });
+
+///////////////////////VALIDAÇÃO DOS CAMPOS///////////////////////
 
 function checkInputFirstName() {
     const firstNameValue = firstName.value.trim();
 
     if (firstNameValue === '') {
         errorInput(firstName, "First name is required");
+        return false;
     } else {
         setSuccess(firstName);
+        return true;
     }
 }
 
@@ -32,8 +38,10 @@ function checkInputLastName() {
 
     if (lastNameValue === '') {
         errorInput(lastName, "Last name is required");
+        return false;
     } else {
         setSuccess(lastName);
+        return true;
     }
 }
 
@@ -43,10 +51,13 @@ function checkInputEmail() {
 
     if (emailValue === '') {
         errorInput(email, "Email is required");
+        return false;
     } else if (!emailRegex.test(emailValue)) {
         errorInput(email, "Email is not valid");
+        return false;
     } else {
         setSuccess(email);
+        return true;
     }
 }
 
@@ -55,12 +66,17 @@ function checkInputPassword() {
 
     if (passwordValue === '') {
         errorInput(password, "Password is required");
+        return false;
     } else if (passwordValue.length < 8) {
         errorInput(password, "Password must be at least 8 characters long");
+        return false;
     } else {
         setSuccess(password);
+        return true;
     }
 }
+
+////////////////////////FUNÇÕES DE VALIDAÇÃO////////////////////////
 
     function isFormValid() {
         const formItems = document.querySelectorAll('.form-item');
@@ -70,14 +86,21 @@ function checkInputPassword() {
     function errorInput(input, message) {
         const formItem = input.parentElement;
         const textMessage = formItem.querySelector('small');
+        const icon = formItem.querySelector('img');
     
         textMessage.innerText = message;
         formItem.classList.remove = ('form-content');
         formItem.classList.add('error');
+        if (icon) icon.style.visibility = 'visible'; // Exibe o ícone de erro
     }
     
     function setSuccess(input) {
         const formItem = input.parentElement;
+        const textMessage = formItem.querySelector('small'); // Limpa a mensagem de erro
+        const icon = formItem.querySelector('img');
+
+        textMessage.innerText = ''; // Limpa a mensagem de erro
         formItem.classList.remove = ('error');
         formItem.classList.add('form-content');
+        if (icon) icon.style.visibility = 'hidden'; // Esconde o ícone de erro
     }
